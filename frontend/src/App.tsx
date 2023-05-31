3d515f08e99b76c207a05a929e9d77ce5ba05d63
Mark <smdmrr00@gmail.com>
2023-05-31 23:14:19 +0200

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './components/navbar';
+import About from './components/about';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+export default function NavBar() {
+  return (
+    <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
+        <a href='/' className='pl-8'>Mark</a>
+        <div className='pr-8 md:block hidden'>
+            <a href='/' className='p-4'>Home</a>
+            <a href='/' className='p-4'>About</a>
+            <a href='/' className='p-4'>Contact</a>
+            <a href='/' className='p-4'>Services</a>
+        </div>
+    </nav>
+  )
+}
